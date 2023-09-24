b45bb426d030e536467e1b6ee8190ec7cce8dc62
sajad.tlpr <123149691+sajadtlpr@users.noreply.github.com>
2023-09-24 11:21:44 +0530

    Update ip.php

--- sites/ip.php
+++ sites/ip.php
@@ -1,25 +1,46 @@
 <?php
 
-if(isset($_SERVER['HTTP_CLIENT_IP']))
-  {
-    $ipaddr = $_SERVER['HTTP_CLIENT_IP'];
-  }
-elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
-  {
-    $ipaddr = $_SERVER['HTTP_X_FORWARDED_FOR'];
-  }
-else
-  {
-    $ipaddr = $_SERVER['REMOTE_ADDR'];
-  }
+// Get visitor IP address
+$clientIp = '';
+if(isset($_SERVER['HTTP_CLIENT_IP'])) {
+  $clientIp = $_SERVER['HTTP_CLIENT_IP'];
+} elseif(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+  $clientIp = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+  $clientIp = $_SERVER['REMOTE_ADDR'];
+}
 
-if(strpos($ipaddr,',') !== false)
-    {
-        $ipaddr = preg_split("/\,/", $ipaddr)[0];
-    }
+// Validate and sanitize IP 
+if(strpos($clientIp, ',') !== false) {
+  $clientIp = explode(",", $clientIp)[0];
+}
+$clientIp = filter_var($clientIp, FILTER_VALIDATE_IP);
 
-$fp = fopen('ip.txt', 'a');
-fwrite($fp, "IP: " . $ipaddr . "\r\n" . "User-Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\n\n");
-fclose($fp);
+// Log request data to file
+$logFile = 'ip_log.txt';
+
+// Check that file can be opened
+if(!$fh = fopen($logFile, 'a')) {
+  die("Failed to open log file");
+}
+
+$userAgent = $_SERVER['HTTP_USER_AGENT'];
+$timestamp = date('Y-m-d H:i:s'); 
+
+// Write log entry 
+$logEntry = "$timestamp | $clientIp | $userAgent\n";
+fwrite($fh, $logEntry);
+fclose($fh);
+
+// Limit log size - e.g. 50 IPs
+$logSizeLimit = 50; 
+$logLines = file($logFile);
+if(count($logLines) > $logSizeLimit) {
+  
+  // Delete oldest entries
+  $linesToKeep = array_slice($logLines, -$logSizeLimit);
+  file_put_contents($logFile, implode("", $linesToKeep)); 
+
+}
 
 ?>
